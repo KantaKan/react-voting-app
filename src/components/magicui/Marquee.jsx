31c654f37ed8f6f3654dbe9323043dcf5466b7b5
fiadaze <fiakantapon@gmail.com>
2025-08-09 04:21:19 +0700

    change some css that not reverse

--- src/components/magicui/Marquee.jsx
+++ src/components/magicui/Marquee.jsx
@@ -10,9 +10,8 @@ export function Marquee({ className, reverse = false, pauseOnHover = false, chil
           key={i}
           className={cn(
             "flex shrink-0 justify-around [gap:var(--gap)]",
-            vertical ? "animate-marquee-vertical flex-col" : "animate-marquee flex-row",
-            pauseOnHover && "group-hover:[animation-play-state:paused]",
-            reverse && "[animation-direction:reverse]"
+            vertical ? "animate-marquee-vertical flex-col" : reverse ? "animate-marquee-reverse flex-row" : "animate-marquee flex-row",
+            pauseOnHover && "group-hover:[animation-play-state:paused]"
           )}
         >
           {children}
