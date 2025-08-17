9b7e4afd83aba78f02a3f6286c9b6fd9ad0d7d2d
fiadaze <fiakantapon@gmail.com>
2025-08-17 21:06:00 +0700

    added some card agian

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,39 @@
+import { MinimalCard, MinimalCardDescription, MinimalCardImage, MinimalCardTitle } from "@/components/ui/minimal-card";
+
+export function ProjectCard() {
+  const cards = [
+    {
+      title: "Sick title",
+      description: "How to design with gestures and motion that feel intuitive and natural.",
+      src: "https://pbs.twimg.com/media/GgMiuRpa4AAoW2y?format=jpg&name=medium",
+    },
+    {
+      title: "Sick title",
+      description: "How to design with gestures and motion that feel intuitive and natural.",
+      src: "https://pbs.twimg.com/media/GgHZJN0aoAA__92?format=jpg&name=medium",
+    },
+    {
+      title: "Sick title",
+      description: "How to design with gestures and motion that feel intuitive and natural.",
+      src: "https://pbs.twimg.com/media/GgCPjsQacAAWvm3?format=jpg&name=medium",
+    },
+  ];
+  return (
+    <div className="w-full max-w-5xl ">
+      <div className="flex flex-col justify-center rounded-lg p-4">
+        <div className="flex flex-wrap items-center justify-center ">
+          {cards.map((card, index) => (
+            <MinimalCard className="m-2 w-[460px] " key={index}>
+              <MinimalCardImage className="h-[320px]" src={card.src} alt={card.title} />
+              <MinimalCardTitle>{card.title}</MinimalCardTitle>
+              <MinimalCardDescription>{card.description}</MinimalCardDescription>
+              <div className="flex m-10">
+                <button>hello</button>
+              </div>
+            </MinimalCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
